3c0f74213d16149056a7d484215e643f55d7970b
ANAND <ClobberXD@gmail.com>
2019-06-28 22:27:18 +0530

    Add pre-compiled header file - src/pch.h

---
+++ src/pch.h
@@ -0,0 +1,51 @@
+/*
+Minetest
+Copyright (C) 2015 est31 <MTest31@outlook.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#pragma once
+
+// C++ standard library
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <memory>
+#include <cmath>
+#include <thread>
+#include <mutex>
+#include <atomic>
+#include <iomanip>
+#include <utility>
+#include <functional>
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+
+
+// Standard Template Library
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <array>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <iterator>
+#include <algorithm>
